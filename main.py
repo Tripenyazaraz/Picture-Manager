6cf6951b9efe1535caff133822e630ae74931dc1
Eldar Allakhyarov <tripenyazaraz@gmail.com>
2023-02-10 10:46:38 +0600

    env

--- main.py
+++ main.py
@@ -81,17 +81,21 @@ async def init_db():
                 },
             },
         }
-        # db_url=f"asyncpg://{PG_DB_USERNAME}:{PG_DB_PASSWORD}@{PG_DB_HOST}:{PG_DB_PORT}/{PG_DB_NAME}",
-        # modules={
-        #     "album": ["src.apps.album.models"],
-        #     "media": ["src.apps.media.models"],
-        #     "tag": ["src.apps.tag.models"],
-        #     "user": ["src.apps.user.models"],
-        # },
-        # generate_schemas=True,
-        # add_exception_handlers=True
     )
-    # Tortoise.init_models(["src.apps.album.models", "src.apps.media.models", "src.apps.tag.models", "src.apps.user.models"], "models")
+    # db_url=f"asyncpg://{PG_DB_USERNAME}:{PG_DB_PASSWORD}@{PG_DB_HOST}:{PG_DB_PORT}/{PG_DB_NAME}",
+    # modules={
+    #     "album": ["src.apps.album.models"],
+    #     "media": ["src.apps.media.models"],
+    #     "tag": ["src.apps.tag.models"],
+    #     "user": ["src.apps.user.models"],
+    # },
+    # generate_schemas=True,
+    # add_exception_handlers=True
+
+    # Tortoise.init_models(["src.apps.user.models"], "user")
+    # Tortoise.init_models(["src.apps.tag.models"], "tag")
+    # Tortoise.init_models(["src.apps.media.models"], "media")
+    # Tortoise.init_models(["src.apps.album.models"], "album")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", port=SERVER_PORT, host="0.0.0.0", reload=True, workers=26)
